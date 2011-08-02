a8f1e535b05bd86a8b3f0a792dfe489115bbb9ec
r-a-y <r-a-y@gmx.com>
2011-08-01 23:26:10 -0700

    Update plugin for 3.1+ compatibility.

--- blue-network-plugins.php
+++ blue-network-plugins.php
@@ -1,14 +1,25 @@
 <?php
 /*
 Plugin Name: Blue Network Plugins!
-Description: Miss how network plugins were separated from regular plugins like in Wordpress MU? Miss the ol' blue network plugins color as well? Check this out!
+Description: Separates active network plugins from inactive ones on the Wordpress network plugins page. Requires Wordpress Multisite.
 Author: r-a-y
-Version: 1.0
+Version: 1.1
+Network: true
 Author URI: http://buddypress.org/community/members/r-a-y
 
 License: CC-GNU-GPL http://creativecommons.org/licenses/GPL/2.0/
 Donate: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6U36PVEZB4BAE
 */
+
+/**
+ * Blue Network Plugins
+ *
+ * @package BNP
+ * @subpackage Core
+ */
+
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
 
 global $wp_version;
 
@@ -16,26 +27,168 @@ global $wp_version;
 if( !is_multisite() && version_compare( $wp_version, '3.0', '<' ) )
 	return false;
 
-// that funky pale blue color you love from WPMU
-// let's only add the CSS on the plugins page
-add_action('admin_head-plugins.php', 'bnp_active_plugin_colors', 99);
-function bnp_active_plugin_colors() {
-?>
-	<style type="text/css">	
-	#network-plugins tr.active th, #network-plugins tr.active td {background-color:#EEF2FF !important;}
-	#network-plugins .check-column input {display:none;}
-	</style>
-<?php	
+/**
+ * Class: Blue Network Plugins
+ *
+ * @package BNP
+ * @subpackage Classes
+ * @since 1.1
+ */
+class Blue_Network_Plugins {
+
+	/**
+	 * Initializes the class when called upon.
+	 * Yeah, I know it's named funny. Why?  See line 177 ;)
+	 */
+	function blue() {
+
+		/* filters **************************************************************/
+
+		// filter network plugins from entire plugins list
+		add_filter( 'all_plugins', 			array( &$this, 'exclude_network_plugins' ) );
+
+		// add extra action links for our plugin
+		add_filter( 'network_admin_plugin_action_links',array( &$this, 'add_plugin_action_links' ), 10, 2 );
+
+		/* actions **************************************************************/
+
+		// localization
+		add_action( 'admin_init', 			array( &$this, 'translate' ) );
+
+		// that funky pale blue color you love from WPMU
+		// let's only add the CSS on the plugins page
+		add_action( 'admin_head-plugins.php',		array( &$this, 'css' ), 99 );
+
+		// add custom network plugins table before the regular plugins table
+		add_action( 'pre_current_active_plugins', 	array( &$this, 'display' ) );
+	}
+
+	/**
+	 * Filter network plugins from entire plugins list
+	 */
+	function exclude_network_plugins() {
+		$all_plugins = get_plugins();
+
+		$network_plugins = ray_get_network_plugins_only();
+		return array_diff_assoc( $all_plugins, $network_plugins );
+	}
+
+	/**
+	 * CSS on the plugins page
+	 */
+	function css() {
+	?>
+		<style type="text/css">
+		#network-plugins tr.active th, #network-plugins tr.active td {background-color:#EEF2FF !important;}
+		#network-plugins .check-column input, h2 .subtitle {display:none;}
+		p.search-box {margin-top:5px;}
+		</style>
+	<?php
+	}
+
+	/**
+	 * Display our modified plugins table above the regular plugin table.
+	 * Accomodates both WP 3.0 and 3.1+.
+	 *
+	 * @uses Network_Plugins_List_Table
+	 */
+	function display() {
+		global $wp_version, $s;
+
+		if ( current_user_can( 'manage_network_plugins' ) ) :
+
+			echo '<h3>' . __( 'Currently Active Network Plugins', 'bnp' ) . '</h3>';
+
+			echo '<div id="network-plugins">';
+
+			// WP 3.1+
+			if ( version_compare( $wp_version, '3.1', '>=' ) ) :
+
+				$screen = get_current_screen();
+
+				// display this on the network admin plugins page
+				if ( $screen->is_network ) :
+					echo '<p>' . __( 'Plugins that appear in the list below are activated on all sites across this installation.', 'bnp' ) .'</p>';
+
+					// WP 3.1 introduces a new class to output plugins
+					// Network_Plugins_List_Table extends WP_Plugins_List_Table
+					$wp_list_table = new Network_Plugins_List_Table;
+					$wp_list_table->prepare_items();
+					$wp_list_table->display();
+
+				// display this on the site admin plugins page
+				else :
+					echo '<p>' . sprintf( __( 'You can <a href="%s">manage your network plugins here</a>.', 'bnp' ), network_admin_url( 'plugins.php' ) ) . '</p>';
+					echo '<h3>' . sprintf( __( 'Plugins on %s', 'bnp' ), get_bloginfo( 'name' ) ) . '</h3>';
+				endif;
+
+			// WP 3.0 branch
+			// print_plugins_table() doesn't exist in WP 3.1+
+			else :
+				echo '<p>' . __( 'Plugins that appear in the list below are activated on all sites across this installation.', 'bnp' ) .'</p>';
+				print_plugins_table( ray_get_network_plugins_only() );
+				echo '<p>' . __( 'Plugins that are enabled on the entire network can only be disabled by a super administrator.', 'bnp' ) .'</p>';
+			endif;
+
+			echo '</div>';
+
+			// if a search is being made, let's reposition the search heading here
+			if ( $s )
+				printf( '<h4>' . __( 'Search results for &#8220;%s&#8221;', 'bnp' ) . '</h3>', esc_html( $s ) );
+
+		endif;
+	}
+
+	/**
+	 * This method works even if we shove the plugin in /mu-plugins/.
+	 */
+	function translate() {
+		$locale = get_locale();
+
+		$mofile = dirname(__FILE__) . "/lang/bnp-$locale.mo";
+
+		if ( file_exists( $mofile ) )
+			load_textdomain( 'bnp', $mofile );
+	}
+
+	/**
+	 * Add extra links for BNP on the WP Admin plugins page
+	 *
+	 * @param array $links Plugin action links
+	 * @param string $file A plugin's loader base filename
+	 */
+	function add_plugin_action_links( $links, $file ) {
+
+		// Do not do anything for other plugins
+		if ( strrpos( 'blue-network-plugins.php', $file ) === false )
+			return $links;
+
+		// Donate link
+		$donate = '<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6U36PVEZB4BAE" target="_blank" title="So you\'re using this plugin, help support this plugin by donating any amount you wish. Thanks for reading! :)" style="font-weight:bold; color:#D54E21">Donate!</a>';
+
+		array_unshift( $links, $donate );
+
+		return $links;
+	}
 }
 
-// filter network plugins from entire plugins list
-add_filter( 'all_plugins', 'ray_exclude_network_plugins' );
-function ray_exclude_network_plugins() {
+// I'm blue, dah bah dee, dah bah dah!
+$eiffel_65 = new Blue_Network_Plugins;
+$eiffel_65->blue();
+
+
+/* FUNCTIONS *************************************************************/
+
+/**
+ * Helper function to get network plugins only
+ *
+ * @since 1.1
+ */
+function ray_get_network_plugins_only() {
 	$all_plugins = get_plugins();
 
 	$network_plugins = array();
 	foreach ( (array) $all_plugins as $plugin_file => $plugin_data) {
-		// Filter into individual sections
 		if ( is_multisite() && is_network_only_plugin( $plugin_file ) && !current_user_can( 'manage_network_plugins' ) ) {
 			unset( $all_plugins[ $plugin_file ] );
 			continue;
@@ -43,35 +196,125 @@ function ray_exclude_network_plugins() {
 			$network_plugins[ $plugin_file ] = $plugin_data;
 		}
 	}
-	return array_diff_assoc($all_plugins, $network_plugins);
+
+	return apply_filters( 'network_plugins_only', $network_plugins );
 }
 
-// add custom network plugins table before the regular plugins table
-// todo: l10n?
-add_action( 'pre_current_active_plugins', 'ray_blue_network_plugins' );
-function ray_blue_network_plugins() {
-	$all_plugins = get_plugins();
 
-	$network_plugins = array();
-	foreach ( (array) $all_plugins as $plugin_file => $plugin_data) {
-		// Filter into individual sections
-		if ( is_multisite() && is_network_only_plugin( $plugin_file ) && !current_user_can( 'manage_network_plugins' ) ) {
-			unset( $all_plugins[ $plugin_file ] );
-			continue;
-		} elseif ( is_plugin_active_for_network($plugin_file) ) {
-			$network_plugins[ $plugin_file ] = $plugin_data;
+/* CLASSES **************************************************************/
+
+// WP 3.1+ support
+if ( version_compare( $wp_version, '3.1', '>=' ) ) :
+
+require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+require_once( ABSPATH . 'wp-admin/includes/class-wp-plugins-list-table.php' );
+
+/**
+ * Class: Network_Plugins_List_Table
+ *
+ * Extends and manipulates {@link WP_Plugins_List_Table class} to
+ * display only network plugins.
+ *
+ * @package BNP
+ * @subpackage Classes
+ * @see Blue_Network_Plugins::display()
+ * @since 1.1
+ */
+class Network_Plugins_List_Table extends WP_Plugins_List_Table {
+
+	/**
+	 * Overrides {@link WP_Plugins_List_Table::prepare_items()}
+	 */
+	function prepare_items() {
+		global $page, $orderby, $order;
+
+		wp_reset_vars( array( 'orderby', 'order', 's' ) );
+
+		$plugins = array(
+			'all' => ray_get_network_plugins_only(),
+			'search' => array(),
+			'active' => array(),
+			'inactive' => array(),
+			'recently_activated' => array(),
+			'upgrade' => array(),
+			'mustuse' => array(),
+			'dropins' => array()
+		);
+
+		$screen = get_current_screen();
+
+		if ( ! is_multisite() || ( $screen->is_network && current_user_can('manage_network_plugins') ) ) {
+			$current = get_site_transient( 'update_plugins' );
+			foreach ( (array) $plugins['all'] as $plugin_file => $plugin_data ) {
+				if ( isset( $current->response[ $plugin_file ] ) )
+					$plugins['upgrade'][ $plugin_file ] = $plugin_data;
+			}
 		}
+
+		set_transient( 'plugin_slugs', array_keys( $plugins['all'] ), 86400 );
+
+		$recently_activated = get_option( 'recently_activated', array() );
+
+		$one_week = 7*24*60*60;
+		foreach ( $recently_activated as $key => $time )
+			if ( $time + $one_week < time() )
+				unset( $recently_activated[$key] );
+		update_option( 'recently_activated', $recently_activated );
+
+		if ( !current_user_can( 'update_plugins' ) )
+			$plugins['upgrade'] = array();
+
+		$totals = array();
+		foreach ( $plugins as $type => $list )
+			$totals[ $type ] = count( $list );
+
+		if ( empty( $plugins[ $status ] ) && !in_array( $status, array( 'all', 'search' ) ) )
+			$status = 'all';
+
+		$this->items = array();
+		foreach ( $plugins[ $status ] as $plugin_file => $plugin_data ) {
+			// Translate, Don't Apply Markup, Sanitize HTML
+			$this->items[$plugin_file] = _get_plugin_data_markup_translate( $plugin_file, $plugin_data, false, true );
+		}
+
+		$total_this_page = $totals[ $status ];
+
+		if ( $orderby ) {
+			$orderby = ucfirst( $orderby );
+			$order = strtoupper( $order );
+
+			uasort( $this->items, array( &$this, '_order_callback' ) );
+		}
+
+		$plugins_per_page = $this->get_items_per_page( str_replace( '-', '_', $screen->id . '_per_page' ) );
+
+		$start = ( $page - 1 ) * $plugins_per_page;
+
+		if ( $total_this_page > $plugins_per_page )
+			$this->items = array_slice( $this->items, $start, $plugins_per_page );
+
+		$this->set_pagination_args( array(
+			'total_items' => $total_this_page,
+			'per_page' => $plugins_per_page,
+		) );
 	}
-?>
-	<h3>Currently Active Network Plugins</h3>
-	<p>Plugins that appear in the list below are activated on all sites across this installation.</p>
 
-	<div id="network-plugins">
-		<?php print_plugins_table($network_plugins); ?>
-	</div>
+	/**
+	 * Overrides {@link WP_Plugins_List_Table::bulk_actions()}
+	 * to return nothing since this isn't applicable for our needs
+	 */
+	function bulk_actions() {
+		return;
+	}
 
-	<p>Plugins that are enabled on the entire network can only be disabled by a super administrator.</p>
-<?php
+	/**
+	 * Overrides {@link WP_Plugins_List_Table::display_tablenav()}
+	 * to return nothing since this isn't applicable for our needs
+	 */
+	function display_tablenav( $which ) {
+		return;
+	}
 }
+endif;
 
 ?>
--- readme.txt
+++ readme.txt
@@ -1,31 +1,32 @@
 === Blue Network Plugins! ===
 Contributors: r-a-y
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6U36PVEZB4BAE
-Tags: 3.0, merge, wpmu, multisite, network, super admin, plugins
+Tags: 3.0, multisite, network, network admin, super admin, plugins
 Requires at least: 3.0
-Tested up to: 3.0+
-Stable tag: 1.0
+Tested up to: 3.2+
+Stable tag: 1.1
 
-Miss how network plugins were separated from regular plugins like in Wordpress MU? Miss the ol' blue network plugins color as well? Check this out!
+Separates active network plugins from inactive ones on the Wordpress network plugins page. Requires Wordpress Multisite.
 
 == Description ==
 
-Wordpress 3.0 is cool like Thelonius Monk, but for super administrators coming from Wordpress MU, if you head on over to the plugins page, you'll notice something missing.
+This plugin makes things a little easier for network administrators managing their network plugins.
 
-Network (or sitewide) plugins are now merged into the entire plugins list!
-
-If you're like me and missed how your network plugins were listed at the very top with the uber-cool blue color, then you'll love this plugin!
+How so?  Glad you asked! Active network plugins are separated from inactive ones on the Wordpress network plugins page.
 
 Need a visual?  Check out [this screenshot](http://i46.tinypic.com/6gv32g.png).
 
-If you're **not** using [network mode](http://codex.wordpress.org/Create_A_Network) in Wordpress 3.0, you *don't* need this plugin.
+For WP 3.1+, if you're on a regular admin plugins page, you'll see a small blurb with an easy, direct link to your network plugins page.
+
+If you're **not** using [network mode](http://codex.wordpress.org/Create_A_Network), you *don't* need this plugin.
 
 
 == Installation ==
 
-#### This plugin requires Wordpress 3.0 in network mode ####
+#### This plugin requires Wordpress to be in [network mode](http://codex.wordpress.org/Create_A_Network) ####
 
-1. Download, install and activate the plugin.
+1. Download and install the plugin.
+1. Navigate to your Network Admin plugins page (usually /wp-admin/network/plugins.php) and activate the plugin. (For WP 3.0, navigate to your regular plugins page and activate.)
 
 I'd recommend putting `blue-network-plugins.php` in /wp-content/mu-plugins/ so all your sites can take advantage of this plugin without manually activating.
 
@@ -33,6 +34,32 @@ I'd recommend putting `blue-network-plugins.php` in /wp-content/mu-plugins/ so a
 == Screenshots ==
 
 A screenshot can be found [here](http://i46.tinypic.com/6gv32g.png).
+
+
+== Frequently Asked Questions ==
+
+#### Why blue? ####
+
+The plugin was originally inspired by Wordpress MU, which used a blue color to identify which plugins were activated network-wide.  Also, in MU, network plugins were also neatly separated from regular site plugins.
+
+When Wordpress 3.0 came about, all plugins were merged together into the regular plugin list.  This made things hard to administrate and hence the genesis of this plugin!
+
+So to answer the question, the blue color is a throwback and tip of the hat to the ol' WPMU days.
+
+The original plugin description is below:
+
+`Wordpress 3.0 is cool like Thelonius Monk, but for super administrators coming from Wordpress MU, if you head on over to the plugins page, you'll notice something missing.
+
+Network (or sitewide) plugins are now merged into the entire plugins list!
+
+If you're like me and missed how your network plugins were listed at the very top with the uber-cool blue color, then you'll love this plugin!`
+
+
+#### Is this plugin even necessary when I'm using WP 3.1+? ####
+
+Good question!  I initially wrote this plugin for WP 3.0 when it was hard to distinguish between a regular plugin and a network plugin.
+
+WP 3.1+ now offers two screens to administrate plugins - one for regular site admin use and another for your network.  It's a little better now, but this plugin is still useful from a usability perspective.  Give it a shot and you'll see! :)
 
 
 == Recommendations ==
@@ -43,6 +70,10 @@ They go well together like coffee and doughnuts!
 
 
 == Changelog ==
+
+= 1.1 =
+* Rewrote the plugin for compatibility with WP 3.1+.
+* Added localization support.
 
 = 1.0 =
 * First version!
